8f00c3486c1833fbc6058db2e8357b4ee8a49880
Aida <ayda.alipoor81.aa@gmail.com>
2025-11-03 01:38:45 +0330

    feat: Add openWeather Configs and Pipline register services

--- src/WiraCodeChallenge.Presentation/Program.cs
+++ src/WiraCodeChallenge.Presentation/Program.cs
@@ -1,6 +1,12 @@
+using WiraCodeChallenge.Application;
+using WiraCodeChallenge.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -10,5 +16,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.MapControllers();
 
 app.Run();
